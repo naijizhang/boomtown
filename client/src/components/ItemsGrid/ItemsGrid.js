2791c7440765d058cf764a9c75789d895759d0c9
Naiji Zhang <naijizhang@gmail.com>
2019-04-25 09:02:44 -0700

    Add ItemsGrid component

---
+++ client/src/components/ItemsGrid/ItemsGrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+
+function ItemsGrid() {
+  return (
+    <Grid>
+     <p>1234567</p>
+   </Grid>
+  );
+}
+  
+  export default ItemsGrid;
+  
+  
