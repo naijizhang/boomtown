4afae5c23e4f7c8e4a3b276f84342bef96e72ae1
Naiji Zhang <naijizhang@gmail.com>
2019-04-25 16:00:04 -0700

    Render itemsgrid on items page

--- client/src/pages/Items/Items.js
+++ client/src/pages/Items/Items.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import ItemsGrid from '../../components/ItemsGrid';
 
-const Items = ({ classes }) => {
+const Items = ({ classes, items }) => {
   return (
     <div>
       <p>
         This is the items page located at <code>/items</code>.
       </p>
+      <ItemsGrid items={items}/>
+      
     </div>
   );
 };
--- client/src/pages/Items/ItemsContainer.js
+++ client/src/pages/Items/ItemsContainer.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import Items from './Items';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
-// import FullScreenLoader from '../../components/FullScreenLoader';
-// import { Query } from 'react-apollo';
-// import { } from '../../apollo/queries';
+//import FullScreenLoader from '../../components/FullScreenLoader';
+import { Query } from 'react-apollo';
+import {ALL_ITEMS_QUERY} from '../../apollo/queries';
 
 class ItemsContainer extends Component {
     render() {
-        return (
-            <Items />
-        );
+      return (
+        <Query query={ALL_ITEMS_QUERY} variables={{filter:1}}>
+          {({ loading, error, data }) => {
+           // if (loading) return <FullScreenLoader inverted />;
+          // console.log(data);
+            if (error) return <p>{`Error! ${error.message}`}</p>;
+            return <Items classes={this.props.classes} items={data.items} />;
+          }}
+        </Query>
+      );
     }
-}
-
-export default withStyles(styles)(ItemsContainer);
+  }
+  
+  export default withStyles(styles)(ItemsContainer);
