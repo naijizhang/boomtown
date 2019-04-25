730ed398fd4fb2297b4241b3810902890474ad5a
Naiji Zhang <naijizhang@gmail.com>
2019-04-25 08:57:48 -0700

    Connect client to apollo

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 // @TODO: Uncomment each module as needed in your client app
-// import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider } from 'react-apollo';
 // import { BrowserRouter } from 'react-router-dom'
 // import { Provider as ReduxProvider } from 'react-redux'
 // -------------------------------
 
 import registerServiceWorker from './registerServiceWorker';
 import theme from './theme';
-
+import client from './apollo';
 /**
  * @TODO: Initialize Apollo Client
  *
@@ -60,13 +60,16 @@ import theme from './theme';
 import Home from './pages/Home';
 // -------------------------------
 
+import ItemsGrid from './components/ItemsGrid'
 import './index.css';
 
 const App = () => {
   return (
     <MuiThemeProvider theme={theme}>
       <CssBaseline />
-      <Home />
+      <ApolloProvider client={client}>
+          <ItemsGrid />
+      </ApolloProvider>
     </MuiThemeProvider>
   );
 };
