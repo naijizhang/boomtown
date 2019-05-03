b4919a082ca0bad4ec0b58aa1916b0f9b31db085
Naiji Zhang <naijizhang@gmail.com>
2019-05-02 19:59:08 -0700

    Add viewer query on server and fix null user error

--- server/api/resolvers/auth.js
+++ server/api/resolvers/auth.js
@@ -51,8 +51,9 @@ module.exports = app => {
           args.user.email
         );
         console.log(user);
+        if( !user) throw 'User was not found.';
         const valid = await bcrypt.compare(password, user.password);
-        if (!valid || !user) throw 'User was not found.';
+        if (!valid) throw 'User / password was wrong.';
 
         setCookie({
           tokenName: app.get('JWT_COOKIE_NAME'),
