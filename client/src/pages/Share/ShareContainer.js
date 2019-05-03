6ce6d6b96d0e4610985c93dc655b910974d0fef4
Naiji Zhang <naijizhang@gmail.com>
2019-05-03 14:37:28 -0700

    Enable full screen loader on share page

--- client/src/pages/Share/ShareContainer.js
+++ client/src/pages/Share/ShareContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Share from './Share';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
-// import FullScreenLoader from '../../components/FullScreenLoader';
+import FullScreenLoader from '../../components/FullScreenLoader';
 import { Query } from 'react-apollo';
 import { ALL_TAGS_QUERY } from '../../apollo/queries';
 
@@ -11,8 +11,7 @@ class ShareContainer extends Component {
     return (
       <Query query={ALL_TAGS_QUERY}>
       {({ loading, error, data }) => {
-       // if (loading) return <FullScreenLoader inverted />;
-        console.log('tags is',data);
+        if (loading) return <FullScreenLoader inverted />;
         if (error) return <p>{`Error! ${error.message}`}</p>;
         return  <Share tags={data.tags? data.tags : []}/>;
       }}
