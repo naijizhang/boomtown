fe68ed555b2168d5452d7cc9bd7d15a71f02e2e5
Naiji Zhang <naijizhang@gmail.com>
2019-05-03 14:37:08 -0700

    Get user from context and pass id to items page

--- client/src/pages/Items/ItemsContainer.js
+++ client/src/pages/Items/ItemsContainer.js
@@ -2,23 +2,27 @@ import React, { Component } from 'react';
 import Items from './Items';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
-//import FullScreenLoader from '../../components/FullScreenLoader';
+import FullScreenLoader from '../../components/FullScreenLoader';
 import { Query } from 'react-apollo';
-import {ALL_ITEMS_QUERY} from '../../apollo/queries';
+import { ALL_ITEMS_QUERY } from '../../apollo/queries';
+import { ViewerContext } from '../../context/ViewerProvider';
+function ItemsContainer({ classes }) {
+  return (
+    <ViewerContext.Consumer>
+      {({ loading, viewer }) => {
+        return (
+          <Query query={ALL_ITEMS_QUERY} variables={{ filter: viewer.id }}>
+            {({ loading, error, data }) => {
+              if (loading) return <FullScreenLoader inverted />;
+              // console.log(data);
+              if (error) return <p>{`Error! ${error.message}`}</p>;
+              return <Items classes={classes} items={data.items} />;
+            }}
+          </Query>
+        );
+      }}
+    </ViewerContext.Consumer>
+  );
+}
 
-class ItemsContainer extends Component {
-    render() {
-      return (
-        <Query query={ALL_ITEMS_QUERY} variables={{filter:2}}>
-          {({ loading, error, data }) => {
-           // if (loading) return <FullScreenLoader inverted />;
-          // console.log(data);
-            if (error) return <p>{`Error! ${error.message}`}</p>;
-            return <Items classes={this.props.classes} items={data.items} />;
-          }}
-        </Query>
-      );
-    }
-  }
-  
-  export default withStyles(styles)(ItemsContainer);
+export default withStyles(styles)(ItemsContainer);
