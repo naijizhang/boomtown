b804ffb5048a771ce07263cedb6145382529885c
Naiji Zhang <naijizhang@gmail.com>
2019-05-04 22:14:31 -0700

    Add user profile query

--- client/src/apollo/queries.js
+++ client/src/apollo/queries.js
@@ -25,7 +25,6 @@ const ItemFields = gql`
   }
 `;
 
-
 // export const ITEM_QUERY = gql`
 //   query item($id: ID!) {
 //     items(filter: $id){
@@ -37,23 +36,25 @@ const ItemFields = gql`
 
 export const ALL_ITEMS_QUERY = gql`
   query items($filter: ID) {
-   items(filter:$filter){
-     ...ItemFields
-   }
+    items(filter: $filter) {
+      ...ItemFields
+    }
   }
   ${ItemFields}
 `;
 
 export const ALL_USER_ITEMS_QUERY = gql`
   query user($id: ID!) {
-    bio
-    email
-    fullname
-    items{
-      ...ItemFields
-    }
-    borrowed{
-      ...ItemFields
+    user(id: $id) {
+      bio
+      email
+      fullname
+      items {
+        ...ItemFields
+      }
+      borrowed {
+        ...ItemFields
+      }
     }
   }
   ${ItemFields}
@@ -61,16 +62,16 @@ export const ALL_USER_ITEMS_QUERY = gql`
 
 export const ALL_TAGS_QUERY = gql`
   query {
-   tags{
-     id
-     title
-   }
+    tags {
+      id
+      title
+    }
   }
 `;
 
 export const ADD_ITEM_MUTATION = gql`
   mutation addItem($item: NewItemInput!) {
-    addItem(item:$item){
+    addItem(item: $item) {
       id
     }
   }
@@ -78,7 +79,7 @@ export const ADD_ITEM_MUTATION = gql`
 
 export const VIEWER_QUERY = gql`
   query {
-    viewer{
+    viewer {
       id
       email
       fullname
@@ -88,22 +89,22 @@ export const VIEWER_QUERY = gql`
 `;
 export const LOGOUT_MUTATION = gql`
   mutation {
-   logout
+    logout
   }
 `;
 
 export const SIGNUP_MUTATION = gql`
   mutation signup($user: SignupInput!) {
-   signup(user:$user){
-     id
-   }
+    signup(user: $user) {
+      id
+    }
   }
 `;
 
 export const LOGIN_MUTATION = gql`
   mutation login($user: LoginInput!) {
-   login(user:$user){
-    id
-   }
+    login(user: $user) {
+      id
+    }
   }
 `;
