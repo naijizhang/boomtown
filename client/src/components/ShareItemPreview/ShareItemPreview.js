17950d0ffd877a22faff5d754c8619aed8631cdd
Naiji Zhang <naijizhang@gmail.com>
2019-05-04 22:17:17 -0700

    Add user picture and user name on share preview

--- client/src/components/ShareItemPreview/ShareItemPreview.js
+++ client/src/components/ShareItemPreview/ShareItemPreview.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import ItemCard from '../../components/ItemCard';
 import { connect } from 'react-redux';
+import { ViewerContext } from '../../context/ViewerProvider';
 const mapStateToProps = ({ shareItemPreview }) => ({
   shareItemPreview
 });
 
 function ShareItemPreview({shareItemPreview}) {
   return (
-    <div>
+    <ViewerContext.Consumer>
+    {({ loading, viewer }) => {
+      return (
       <ItemCard
-        item={shareItemPreview}
+        item={{...shareItemPreview,
+        itemowner:{
+          fullname: viewer.fullname,
+          email: viewer.email
+        }}}
         breakpoints={{ xs: 12, sm: 12, md: 12, lg: 12 }}
       />
-    </div>
+     );
+    }}
+  </ViewerContext.Consumer>
   );
 }
 export default connect(mapStateToProps)(ShareItemPreview);
