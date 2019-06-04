706e46434b78249cb444c38a55fff2a178a693cf
Robin Zhang <robinzhang@Robins-MacBook-Pro.local>
2019-06-04 10:39:22 -0700

    Add current user id on profile page url

--- client/src/components/HeaderMenu/HeaderMenu.js
+++ client/src/components/HeaderMenu/HeaderMenu.js
@@ -15,6 +15,7 @@ import IconButton from '@material-ui/core/IconButton';
 import { Link } from 'react-router-dom';
 import { LOGOUT_MUTATION, VIEWER_QUERY } from '../../apollo/queries';
 import { graphql, compose } from 'react-apollo';
+import { ViewerContext } from '../../context/ViewerProvider';
 class HeaderMenu extends Component {
   state = {
     anchorEl: null
@@ -32,75 +33,85 @@ class HeaderMenu extends Component {
     const { classes, logoutMutation } = this.props;
     const path = window.location.pathname;
     return (
-      <div className={classes.root}>
-        <AppBar position="fixed">
-          <Toolbar className={classes.topToolBar}>
-            <IconButton component={Link} to="/items">
-              <img src={logo} alt="Boomtown logo" className={classes.logo} />
-            </IconButton>
-            <div className={classes.grow} />
-            {path === '/share' ? null : (
-              <Button
-                variant="text"
-                color="primary"
-                disableFocusRipple={true}
-                disableRipple={true}
-                className={classes.buttonLeft}
-                component={Link}
-                to="/share"
-              >
-                <AddIcon className={classes.leftAddIcon} />
-                SHARE SOMETHING
-              </Button>
-            )}
+      <ViewerContext.Consumer>
+        {({ loading, viewer }) => {
+          return (
+            <div className={classes.root}>
+              <AppBar position="fixed">
+                <Toolbar className={classes.topToolBar}>
+                  <IconButton component={Link} to="/items">
+                    <img
+                      src={logo}
+                      alt="Boomtown logo"
+                      className={classes.logo}
+                    />
+                  </IconButton>
+                  <div className={classes.grow} />
+                  {path === '/share' ? null : (
+                    <Button
+                      variant="text"
+                      color="primary"
+                      disableFocusRipple={true}
+                      disableRipple={true}
+                      className={classes.buttonLeft}
+                      component={Link}
+                      to="/share"
+                    >
+                      <AddIcon className={classes.leftAddIcon} />
+                      SHARE SOMETHING
+                    </Button>
+                  )}
 
-            <div>
-              <Button
-                variant="text"
-                color="primary"
-                disableFocusRipple={true}
-                disableRipple={true}
-                className={classes.buttonRight}
-                aria-owns={anchorEl ? 'simple-menu' : undefined}
-                aria-haspopup="true"
-                onClick={this.handleClick}
-              >
-                <MoreVertIcon />
-              </Button>
-              <Menu
-                id="simple-menu"
-                anchorEl={anchorEl}
-                open={Boolean(anchorEl)}
-                onClose={this.handleClose}
-              >
-                <MenuItem
-                  onClick={this.handleClose}
-                  component={Link}
-                  to="/profile"
-                >
-                  <img
-                    src={fingerLogo}
-                    alt="profile logo"
-                    className={classes.menulogo}
-                  />Profile
-                </MenuItem>
-                <MenuItem
-                  onClick={() => {
-                    logoutMutation().catch(error => window.alert(error));
-                    this.setState({ anchorEl: null });
-                  }}
-                >
-                  <img
-                    src={powerLogo}
-                    alt="log out logo"
-                    className={classes.menulogo}
-                  />Logout
-                </MenuItem>
-              </Menu>
+                  <div>
+                    <Button
+                      variant="text"
+                      color="primary"
+                      disableFocusRipple={true}
+                      disableRipple={true}
+                      className={classes.buttonRight}
+                      aria-owns={anchorEl ? 'simple-menu' : undefined}
+                      aria-haspopup="true"
+                      onClick={this.handleClick}
+                    >
+                      <MoreVertIcon />
+                    </Button>
+                    <Menu
+                      id="simple-menu"
+                      anchorEl={anchorEl}
+                      open={Boolean(anchorEl)}
+                      onClose={this.handleClose}
+                    >
+                      <MenuItem
+                        onClick={this.handleClose}
+                        component={Link}
+                        to={'/profile/' + viewer.id}
+                      >
+                        <img
+                          src={fingerLogo}
+                          alt="profile logo"
+                          className={classes.menulogo}
+                        />Profile
+                      </MenuItem>
+                      <MenuItem
+                        onClick={() => {
+                          logoutMutation().catch(error => window.alert(error));
+                          this.setState({ anchorEl: null });
+                        }}
+                      >
+                        <img
+                          src={powerLogo}
+                          alt="log out logo"
+                          className={classes.menulogo}
+                        />Logout
+                      </MenuItem>
+                    </Menu>
+                  </div>
+                </Toolbar>
+              </AppBar>
             </div>
-          </Toolbar>
-        </AppBar>
-      </div>
+          );
+        }}
+      </ViewerContext.Consumer>
     );
   }
 }
